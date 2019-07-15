41808d3d014ab144547bef2102e5dab4ac96f6fb
33haji <one_for_all_0828@yahoo.co.jp>
2019-07-15 19:39:49 +0900

    GitHubのログイン処理などを実装

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # E2E-sample
 CodeceptJS × PuppeteerでのE2Eテスト
+
+## E2Eテストの実行方法
+* 環境変数にGitHubのログインに関する情報をセット
+```
+$ export E2E_USERNAME=[GitHubのusername]
+$ export E2E_PASSWORD=[GitHubのpassword]
+```
+
+* 以下のコマンドを実行
+```
+$ node node_modules/.bin/codeceptjs run --steps
+```
--- init_test.js
+++ init_test.js
@@ -1,7 +1,20 @@
-
 Feature('Init test.js');
 
-Scenario('test something', (I) => {
+Scenario('サイトへアクセス', (I) => {
   I.amOnPage('https://hafilog.com/');
   I.see('HAFILOG');
+});
+
+Scenario('GitHubにログイン', (I) => {
+  const username = process.env.E2E_USERNAME;
+  const password = process.env.E2E_PASSWORD;
+  if (!username || !password) throw new Error('環境変数にE2E_USERNAMEかE2E_PASSWORD、または両方が設定されていません')
+
+  I.amOnPage('https://github.com/login');
+  I.see('Sign in to GitHub', 'h1');
+  I.fillField('Username or email address', username);
+  I.fillField('Password', password);
+  I.click('Sign in');
+  I.dontSee('Incorrect username or password.', '.flash-error');
+  // I.waitUrlEquals('https://github.com/');
 });
